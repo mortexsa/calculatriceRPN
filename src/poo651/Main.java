30360c7c15e14ff9eba0bdce10dd52ead191e090
mortexsa <slimani-arezki@hotmail.com>
2018-11-11 11:19:31 +0100

    ajout de la saisieRPN, il manque plus que les exceptions

--- src/poo651/Main.java
+++ src/poo651/Main.java
@@ -4,10 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MoteurRPN m = new MoteurRPN();
-		m.enregistrer(12);
-		m.enregistrer(13);
-		System.out.println(m);
+		SaisieRPN saisieRPN = new SaisieRPN();
+		saisieRPN.entrerSaisie();
 		
 	}
 
