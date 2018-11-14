e1fb932fa41c1a06f6bae275bc2339c86caadd19
Mariane Tarabay <tarabaymariane@gmail.com>
2018-11-14 20:14:24 +0100

    petits changements

--- src/poo651/SaisieRPN.java
+++ src/poo651/SaisieRPN.java
@@ -1,57 +1,49 @@
 package poo651;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class SaisieRPN {
 	public static final double MIN_VALUE = 0.000001;
 	public static final double MAX_VALUE = 10000000;
-	
+
 	public void entrerSaisie() throws Exception {
 		MoteurRPN moteurRPN = new MoteurRPN();
 		double operande;
-		
+
 		System.out.println("entrez un nombre ou un operande ou quittez en ecrivant 'exit':");
 		Scanner scan = new Scanner(System.in);
-		
+
 		String input = scan.nextLine();
-		
-		while(!input.equals("exit")) {
+
+		while (!input.equals("exit")) {
 			try {
-				if(input.length()==0)
+				if (input.length() == 0)
 					throw new HorsBornesException("L'input ne peut pas etre vide");
-				if(input.equals("+")) {
+				if (input.equals("+")) {
 					moteurRPN.appliquer(Operation.PLUS);
-				} else if(input.equals("-")) {
+				} else if (input.equals("-")) {
 					moteurRPN.appliquer(Operation.MOINS);
-				} else if(input.equals("*")) {
+				} else if (input.equals("*")) {
 					moteurRPN.appliquer(Operation.MULT);
-				} else if(input.equals("/")) {
+				} else if (input.equals("/")) {
 					moteurRPN.appliquer(Operation.DIV);
 				} else {
 					operande = Double.parseDouble(input);
-					if(Math.abs(operande) >= MIN_VALUE && Math.abs(operande) <= MAX_VALUE) {
+					if (Math.abs(operande) >= MIN_VALUE && Math.abs(operande) <= MAX_VALUE) {
 						moteurRPN.enregistrer(operande);
-					}
-					else
-						throw new HorsBornesException("La valeur doit etre un nombre entre "+MIN_VALUE+" et "+MAX_VALUE);					
+					} else
+						throw new HorsBornesException(
+								"La valeur doit etre un nombre entre " + MIN_VALUE + " et " + MAX_VALUE);
 				}
-			}catch (PileException e) {
+			} catch (PileException e) {
 				System.out.println(e.getMessage());
-			}
-			catch (NonDivisibleException e)
-			{
+			} catch (NonDivisibleException e) {
 				System.out.println(e.getMessage());
-			}
-			catch(HorsBornesException e)
-			{
+			} catch (HorsBornesException e) {
 				System.out.println(e.getMessage());
-			}
-			catch(NumberFormatException e)
-			{
+			} catch (NumberFormatException e) {
 				System.out.println("Le input doit etre un nombre ou un symbole d'operation");
-			}
-			finally {
+			} finally {
 				System.out.println(moteurRPN.toString());
 				input = scan.nextLine();
 			}
